b916e838e3faaba672ddd90720266f4de6fa11f0
Sahil Shinde <sahilshinde9730@gmail.com>
2025-03-14 22:40:22 +0530

    experience section done

--- my-portfolio/src/component/experienceSection/AllExperience.jsx
+++ my-portfolio/src/component/experienceSection/AllExperience.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+ import React from 'react'
+import SingleExperience from './SingleExperience'
+import { FaArrowRight } from "react-icons/fa";
+
+const experiences = [
+  {
+    job : 'Front-End Developer',
+    company:'Alex Apps',
+    date:'2024-Present',
+    responsibilities :[
+      '•I specialize in React and modern JavaScriptReact and modern JavaScript, ',
+      '•leveraging best practices to create scalable and maintainable solutions.',
+      
+    ]
+  },
+  {
+    job : 'Front-End Developer',
+    company:'Alex Apps',
+    date:'2024-Present',
+    responsibilities :[
+      '•I specialize in React and modern JavaScriptReact and modern JavaScript, ',
+      '•leveraging best practices to create scalable and maintainable solutions.',
+      
+      
+    ]
+  },
+  {
+    job : 'Front-End Developer',
+    company:'Alex Apps',
+    date:'2024-Present',
+    responsibilities :[
+      '•I specialize in React and modern JavaScriptReact and modern JavaScript, ',
+      '•leveraging best practices to create scalable and maintainable solutions.',
+      
+    ]
+  },
+]
 
 const AllExperience = () => {
   return (
-    <div>AllExperience</div>
+    <div className='flex md:flex-row sm:flex-col items-center justify-between'>
+      {experiences.map((experience ,index)=>{
+        return <>
+        <SingleExperience key={index} experience={experience}/>
+        {index < 2 ? <FaArrowRight className='text-6xl text-orange lg:block sm:hidden'/> : ""}
+        
+        </> 
+      })}
+    </div>
   )
 }
 
-export default AllExperience
+export default AllExperience 
--- my-portfolio/src/component/experienceSection/SingleExperience.jsx
+++ my-portfolio/src/component/experienceSection/SingleExperience.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const SingleExperience = () => {
+const SingleExperience = ({ experience }) => {
   return (
-    <div>SingleExperience</div>
-  )
-}
+    <div className=" md:h-[300px] md:w-[240px] sm:h-auto sm:w-full border-2 border-orange border-dashed rounded-2xl mt-12 p-4 ">
+      <p className="font-bold text-cyan">{experience.job}</p>
+      <p className="text-orange">{experience.company}</p>
+      <p className="text-lightGray">{experience.date}</p>
+      <ul className="list-disk mt-4 pl-4 text-white ">
+        {experience.responsibilities.map((resp, index) => {
+          return <li key={index}>{resp}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default SingleExperience
+export default SingleExperience;
+
+//md:h-[300px] md:w-[240px] sm:h-auto sm:w-auto border-2 border-orange border-dashed rounded-2xl mt-12 p-4
