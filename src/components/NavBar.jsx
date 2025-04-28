d081973528645cea9f20dcdf93eb9a5de6fb57a7
Your Name <your.email@example.com>
2025-04-28 22:10:56 +0530

    change  in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,49 +1,49 @@
 import { useEffect, useState } from "react";
 import { navLinks } from "../constants/index.js";
 
+
 const NavBar = () => {
-    const [scrolled, setScrolled] = useState(false);
+    
+    const [scrolled , setScrolled] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
             const isScrolled = window.scrollY > 10;
-            setScrolled(isScrolled); // set state to true or false based on scroll position
+            setScrolled(true)
         }
+        window.addEventListener('scroll', handleScroll);
 
-        window.addEventListener('scroll', handleScroll, { passive: true }); // add event listener with passive true for performance
+        return () => window.removeEventListener('scroll',handleScroll);
+    },[])
+   return (
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
+      <div className="inner">
+        <a className="logo" href="#hero">
+          Sahil | Shinde
+        </a>
 
-        // Cleanup on component unmount
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);  // Empty dependency array ensures it runs once when component mounts
+        <nav className="desktop">
+            <ul>
+                {navLinks.map(({link, name}) => (
+                    <li key={name} className="group">
+                        <a href={link}>
+                            <span>{name}</span>
+                            <span className="underline"/>
+                        </a>
 
-    return (
-        <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
-            <div className="inner">
-                <a className="logo" href="#hero">
-                    Sahil | Shinde
-                </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
 
-                <nav className="desktop">
-                    <ul>
-                        {navLinks.map(({ link, name }) => (
-                            <li key={name} className="group">
-                                <a href={link}>
-                                    <span>{name}</span>
-                                    <span className="underline" />
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-
-                <a href="#contact" className="contact-btn group">
-                    <div className="inner">
-                        Contact me
-                    </div>
-                </a>
-            </div>
-        </header>
-    );
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            Contact me
+          </div>
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
